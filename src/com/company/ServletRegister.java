18117d79a306504d7b4df47085190a0712c6c199
lemisky <1478449280@qq.com>
2020-01-26 12:31:06 +0800

    去除jsp相关文件

---
+++ src/com/company/ServletRegister.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "ServletRegister", urlPatterns = {"/ServletRegister"})
+public class ServletRegister extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String user = request.getParameter("user");
+        String password = request.getParameter("password");
+        UserObject userObject = new UserObject(user, password);
+        String result;
+        if (SqlLiteHelper.insert(userObject)) {
+            //注册成功，前往登录
+            result = "<a href='login.html'>注册成功，前往登录</a>";
+        } else {
+            //注册失败，重新注册
+            result = "<a href=’register.html‘>注册成功，前往登录</a>";
+        }
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter out = response.getWriter();
+        out.println(result);
+    }
+
+}
